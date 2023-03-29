48a2bdac01886c9b9dd8d3bb0ed8751e49b94a3e
ortiz-da <ortiz.da@northeastern.edu>
2023-03-29 18:21:00 -0400

    3.3 and 3.4 done

--- controllers/users/user-controller.js
+++ controllers/users/user-controller.js
@@ -3,6 +3,13 @@ let users = people;
 
 const UserController = (app) => {
     app.get('/api/users', findUsers)
+    app.get('/api/users/:uid', findUserById)
+}
+
+const findUserById = (req, res) => {
+    const userId = req.params.uid;
+    const user = users.find(u => u._id === userId);
+    res.json(user);
 }
 
 const findUsers = (req, res) => {
