f0d4fdf62d0428aea993bde3f992c8c0bfb68253
ortiz-da <ortiz.da@northeastern.edu>
2023-03-30 22:56:48 -0400

    4.6 done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/user-controller.js";
 
 const app = express()
+app.use(express.json());
+
 HelloController(app)
 UserController(app)
 app.listen(4000)
