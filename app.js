8e1d39d18990b5608ea903d481d78456ff9ae718
ortiz-da <ortiz.da@northeastern.edu>
2023-03-31 22:34:46 -0400

    5.5 done

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/user-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
 
 const app = express()
 app.use(express.json());
 
 HelloController(app)
 UserController(app)
+TuitsController(app)
 app.listen(4000)
