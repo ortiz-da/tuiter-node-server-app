4f01bd7de28f4c1c4e380e993ff7804b92f8f2c9
ortiz-da <ortiz.da@northeastern.edu>
2023-04-06 21:48:41 -0400

    3.2 done

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/user-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-
+import mongoose from "mongoose";
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 const app = express()
 
 app.use(cors())
